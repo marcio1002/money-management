79e5c3ce09dd054e77ce6d032c9990859b5e3506
marcio1002 <marcioalemao1999@yahoo.com>
2023-10-25 10:28:24 -0300

    init: First commit to repository 🎉

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+/* --- global styles --- */
+import '@styles/tailwind.css';
+
+function App() {
+  return (
+    <div className='w-12/12 bg-white flex justify-center align-center'>
+      <div className='mx-auto'>
+        <button>Ok</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
